fc3db3befd51208fd75af3213e997fa4083d08c2
danielpirela <danielapirelac@gmail.com>
2024-03-02 14:47:21 -0800

    fix media links

--- src/utils/social.ts
+++ src/utils/social.ts
@@ -8,11 +8,11 @@ export const social: Social[] = [
   {
     name: "TikTok",
     src: "/images/tk.webp",
-    href: "https://www.tiktok.com/@zmrclub?_t=8jzXdxTZfNu&_r=1",
+    href: "https://www.tiktok.com/@zmrclub",
   },
   {
     name: "Instagram",
     src: "/images/ig.webp",
-    href: "https://www.instagram.com/zmr.club?igsh=MWxmbmJyMDRqamp5eQ%3D%3D&utm_source=qr",
+    href: "https://www.instagram.com/zmr.club",
   },
 ]
