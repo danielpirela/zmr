56c13f27daddbba302cb416ca5185707ac348900
danielpirela <danielapirelac@gmail.com>
2024-03-02 17:44:52 -0800

    add about

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -51,7 +51,7 @@ export function Header() {
                         </li>
                         <li>
                             <a
-                                href='/#about'
+                                href='/about'
                                 className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent  hover:text-primary hover:scale-110 transition-all duration-200'
                             >
                                 About
