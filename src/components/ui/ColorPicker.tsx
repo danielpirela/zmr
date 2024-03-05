3b6b0130bf300eb53976f4191123a79eb15ced30
danielpirela <danielapirelac@gmail.com>
2024-03-04 17:36:34 -0800

    some fix

--- src/components/ui/ColorPicker.tsx
+++ src/components/ui/ColorPicker.tsx
@@ -31,7 +31,7 @@ export const ColorPicker = ({avaliableModels}: Props) => {
                             Disponible:
                             </p>
                             <p
-                            className={`rounded-full bg-[${color.hex}] ring-1 ring-gray-900/35 block min-w-5 min-h-5 hover:scale-110 hover:ring-blue-500/50 hover:ring-2 transition-all duration-300 delay-200`}
+                            className={`rounded-full bg-[#000] ring-1 ring-gray-900/35 block min-w-5 min-h-5 hover:scale-110 hover:ring-blue-500/50 hover:ring-2 transition-all duration-300 delay-200`}
                             >
                             </p>
                         </div>
