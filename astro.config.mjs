02fc0fe38b291beb90ed3b6799e85e084f1e5745
danielpirela <danielapirelac@gmail.com>
2024-06-24 22:34:30 -0800

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import million from 'million/compiler';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
+import vercelServerless from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +16,7 @@ export default defineConfig({
       auto: true
     })],
     output: 'server'
-  }
+  },
+  output: "server",
+  adapter: vercelServerless()
 });
