95a20d0203af81b97c358624bd8862145e0e9cf6
danielpirela <danielapirelac@gmail.com>
2024-06-24 22:21:52 -0800

    fix call db

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -3,4 +3,4 @@ const supabaseUrl = import.meta.env.SUPABASE_URL
 const supabaseKey = import.meta.env.SUPABASE_KEY
 
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(`${supabaseUrl}`, `${supabaseKey}`)
