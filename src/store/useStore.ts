5b439e5865d580dbd9161962ecedd7f57fb6441b
danielpirela <danielapirelac@gmail.com>
2024-06-24 20:40:43 -0800

    fix 2 store

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -1,16 +1,10 @@
 import { map } from "nanostores"
 import { persistentMap } from "@nanostores/persistent"
-import type { BooleanLiteral } from "typescript"
-import { string } from "astro/zod"
 // ? Types for slices
 
 interface Product {
   category: string
   id: string
-}
-
-export interface IsLogged {
-  value: "true" | "false"
 }
 
 export const $isLogged = persistentMap("isLogged:", {
