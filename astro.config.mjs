3decb8cbafd910220e8b004265b2035c05a7611c
danielpirela <danielapirelac@gmail.com>
2024-06-24 22:35:41 -0800

    del vercel only

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 import million from 'million/compiler';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
-import vercel from "@astrojs/vercel/serverless";
 import vercelServerless from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
